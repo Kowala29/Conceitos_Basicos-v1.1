3756897f2f24611b458ef49d94c44aef67ba81fb
Kowala29 <eduassis29@gmail.com>
2023-11-14 16:47:29 -0300

    Descrição da ativiade no Programs.cs

---
+++ AulasPOO/Program.cs
@@ -0,0 +1,6 @@
+﻿/*
+ * O projeto devera conter uma classe cliente que pussiu nome, endereço, telefone e data de nascimento
+ * a classe mcliente será criada dentro de uma pasta chamada Model
+ * Deverá ser possivel verificar a idade do cliente a partir de um método da própria classe
+ */ 
+
---
+++ TestesAulasPOO/UnitTest1.cs
@@ -0,0 +1,20 @@
+namespace TestesAulasPOO
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestaConstrutorDefaultCliente()
+        {
+            //Este código verifica a crianção de instancias da classe cliente pelo construtor default
+
+        }
+
+        [TestMethod]
+        public void TestaConstrutorClienteComNome()
+        {
+            //Este código verifica a crianção de instancias da classe cliente informando o seu nome
+
+        }
+    }
+}
