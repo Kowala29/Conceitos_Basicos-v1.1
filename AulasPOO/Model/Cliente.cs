96df2b14810654a1b1f41db2ea552cf1b739edb0
Eduardo de Assis <eduassis29@gmail.com>
2023-11-21 00:14:23 -0300

    Classe com nome, endereço, telefone e data de nascimento do cliente, metodo da classe para verificar a idade do cliente

---
+++ AulasPOO/Model/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulasPOO.Model {
+    public class Cliente {
+        public static int IdadeCliente(DateTime dataNacimento) {
+            int idade = DateTime.Now.Year - dataNacimento.Year;
+            if (DateTime.Now.DayOfYear < dataNacimento.DayOfYear) {
+                idade = idade - 1;
+            }
+            return idade;
+        }
+    }
+}
--- AulasPOO/Program.cs
+++ AulasPOO/Program.cs
@@ -2,5 +2,20 @@
  * O projeto devera conter uma classe cliente que pussiu nome, endereço, telefone e data de nascimento
  * a classe mcliente será criada dentro de uma pasta chamada Model
  * Deverá ser possivel verificar a idade do cliente a partir de um método da própria classe
- */ 
+ */
 
+using AulasPOO.Model;
+
+Console.WriteLine("Entre com o Nome:");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Entre com o Endereço:");
+string endereco = Console.ReadLine();
+
+Console.WriteLine("Enre com o telefone:");
+int telefone = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Entre com a Data de Nascimento:");
+DateTime dataNascimento = DateTime.Parse(Console.ReadLine());
+
+Console.WriteLine(Cliente.IdadeCliente(dataNascimento));
